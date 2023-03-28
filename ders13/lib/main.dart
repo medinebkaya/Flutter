cd7864f75224f5897a7c9f9f37242b0ec0b9ed89
Medine Burunkaya <medineburunkaya032@gmail.com>
2023-03-28 14:50:51 +0300

    Simple To Do App

--- ders13/lib/main.dart
+++ ders13/lib/main.dart
@@ -7,6 +7,90 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: "Hesap Makinesi",
+      home: Iskele() ,
+    );
   }
 }
+
+class Iskele extends StatelessWidget {
+  const Iskele({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Basit Hesap Makinesi"),
+      ),
+      body: AnaEkran(),
+    );
+  }
+}
+class AnaEkran extends StatefulWidget {
+  const AnaEkran({Key? key}) : super(key: key);
+
+  @override
+  State<AnaEkran> createState() => _AnaEkranState();
+}
+
+class _AnaEkranState extends State<AnaEkran> {
+  num sayi1=0, sayi2=0, sonuc=0;
+  TextEditingController t1=TextEditingController();
+  TextEditingController t2=TextEditingController();
+
+  sayiTopla(){
+    setState(() {
+      sayi1=num.parse(t1.text);
+      sayi2=num.parse(t2.text);
+      sonuc=sayi1+sayi2;
+    });
+  }
+
+  sayiCikar(){
+    setState(() {
+      sayi1=num.parse(t1.text);
+      sayi2=num.parse(t2.text);
+      sonuc=sayi1-sayi2;
+    });
+  }
+
+  sayiCarp(){
+    setState(() {
+      sayi1=num.parse(t1.text);
+      sayi2=num.parse(t2.text);
+      sonuc=sayi1*sayi2;
+    });
+  }
+
+  sayiBol(){
+    setState(() {
+      sayi1=num.parse(t1.text);
+      sayi2=num.parse(t2.text);
+      sonuc=sayi1/sayi2;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(50.0),
+      child: Center(
+            child: Column(children: [
+              Text(sonuc.toString()),
+              TextField(
+                controller: t1,
+              ),
+              TextField(
+                controller: t2,
+              ),
+              ElevatedButton(onPressed: sayiTopla, child: Text("Topla")),
+              ElevatedButton(onPressed: sayiCikar, child: Text("Çıkar")),
+              ElevatedButton(onPressed: sayiCarp, child: Text("Çarp")),
+              ElevatedButton(onPressed: sayiBol, child: Text("Böl"))
+
+            ],
+            ),
+      ),
+      );
+  }
+}
+
